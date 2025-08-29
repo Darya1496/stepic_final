6a821aa77b835d4281075885d18d162c94c034ac
Дарья Промзелева <daryapromzeleva@MacBook-Pro-Dara.local>
2025-08-29 20:11:47 +0300

    Запуск теста с параметризацией и пометка теста с багом

--- tests/test_guest_can_add_product_to_basket.py
+++ tests/test_guest_can_add_product_to_basket.py
@@ -1,11 +1,25 @@
 import time
+
+import pytest
+
 from pages.shop.product_page import ProductPage
 from pages.main_page import MainPage
 from pages.base_page import BasePage
 
-def test_add_page(browser):
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                   marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_add_page(browser,link):
     #Открываем страницу товара
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = MainPage(browser, link)
     page.open()
 
@@ -28,6 +42,7 @@ def test_add_page(browser):
     as_book.get_name()
     as_book.get_name_alert()
     as_book.should_be_same_name()
+    time.sleep(10)
 
     print("Названия совпадают!")
 
